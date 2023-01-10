d6a1dd9f27dc36734920111dbfecb9826ed7778b
FiJislav <fiji.cs@seznam.cz>
2023-01-10 17:12:55 +0100

    merge

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,19 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+cislo=input("Zadej cislo: ").strip()
+
+if cislo.isdigit() == True:
+    cislo = int(cislo)
+    if cislo > 0:
+        for i in range(0,100):
+            print(i)
+    elif cislo == 0:
+        print("Cislo je 0")
+    elif cislo < 0:
+        for i in range(100, -1, -1):
+            print(i)
+    else:
+        for i in range(100,0, -1):
+            print(i)
+else:
+    print("neni cislo")
 
 
-"""a
-awdaw Awd 
-
-
-"""
